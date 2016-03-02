e58a82fcffb3969e84b3f1598e0ee269b9614807
leninwebdeveloper <ingeleninfernando@gmail.com>
2016-03-02 16:52:11 -0500

    newbranch

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,12 +1,36 @@
 class ActorsController < ApplicationController
 
 
+
+
 def index
 
   @actors = Actor.all
 
 
+end
+
+
+def new 
+
 
 
 end
+
+def create
+
+ render plain: params[:actor].inspect
+
+
+
+end
+
+#private
+ 
+ # def article_params
+  #  params.require(:actor).permit(:bio, :name, :birth_date, :birth_place, :image_url, :death_date, :death_place)
+  #end
+
+
+
 end
