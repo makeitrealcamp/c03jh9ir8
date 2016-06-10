2fff118bda446ed3a0a79938b91279b4575e524b
Edgar Quintana <edgarquintana984@hotmail.com>
2016-06-10 13:41:28 -0500

    New Actor Management (Form, controller)

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,4 +4,22 @@ class ActorsController < ApplicationController
     @actors = Actor.all
   end
 
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      flash[:notice] = "The actor was successfully created"
+      redirect_to actors_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def actor_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+  end
 end
