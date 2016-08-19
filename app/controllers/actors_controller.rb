cc120a2c66de7ca289dc73c516a1d35b8f2aeac4
hermesagm <hermesagm@gmail.com>
2016-08-19 14:53:07 -0500

    Se crea la ruta new y boton crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,4 +8,16 @@ class ActorsController < ApplicationController
   @actor = Actor.new	
  end
 
+ def create
+   @actor = Actor.new(actor_params)	
+   if @actor.save
+     redirect_to actors_path, notice: "El Actor Fue publicado con éxito"	
+   end
+ end
+
+ private
+   def actor_params
+     params.required(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+   end
+
 end
