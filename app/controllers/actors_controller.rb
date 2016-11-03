db7f775d8a063113b37e193badefa44cc1d22e26
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-11-02 20:55:02 -0500

    Se agrega la acción crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,27 @@
 class ActorsController < ApplicationController
 
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      flash[:notice] = 'The actor have been saved'
+      redirect_to actors_path
+    else
+      render :new
+    end
+  end
+
   def index
     @actors = Actor.all
   end
+
+  private
+
+  def actor_params
+    params.require(:actor).permit(:name,:bio,:birth_date,:birth_place,:image_url,:alive,:death_date,:death_place)
+  end
+
 end
