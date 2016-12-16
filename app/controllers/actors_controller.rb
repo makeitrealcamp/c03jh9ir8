e0a29fd2b62102234771f99b638234b243fa810e
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2016-12-16 08:25:05 -0500

    crear producto

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -6,4 +6,28 @@ def index
 	
 end
 
+def new
+@actor = Actor.new
+
+end
+
+def create
+@actor = Actor.new(actor_params)
+
+if @actor.save
+	redirect_to actors_path , notice: "El actor fue creado correctamente"
+else
+
+	render :new
+    end
+
+end
+
+
+private
+  def actor_params
+    params.require(:actor).permit(:name, :image_url, :bio, :birth_date, :birth_place, :death_place, :death_date, :alive)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  get "/actors", to: "actors#index"
+  get "actors", to: "actors#index"
+  get "actor/new", to: "actors#new" , as: "new_actor"
+  post "actors" , to: "actors#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
