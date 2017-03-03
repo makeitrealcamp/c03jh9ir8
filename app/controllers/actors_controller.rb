fe89f87bc0011958bcb8cadd29de5847f129aad9
Daniel Tobon <shiriux1@gmail.com>
2017-03-03 11:10:55 -0500

    crear-actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,19 @@ class ActorsController < ApplicationController
 def index
 @actors = Actor.all
 end
+
+def new
+	@actor = Actor.new
+	
+end
+
+def create
+actor = Actor.create(actor_params)
+redirect_to actors_path
+end
+
+def actor_params
+	params.require(:actor).permit(:name,:bio,:birth_date,:birth_place,:image_url,:death_date,:death_place)
+	
+end
 end
