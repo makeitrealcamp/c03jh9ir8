6e125062068be2b15e90890d4b69929d9948b158
Jorge Jimenez <stellajimenez@Apple-Mac.local>
2017-03-10 20:21:01 -0500

    create actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,4 +4,23 @@ class ActorsController < ApplicationController
     @actors = Actor.all
   end
 
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+     redirect_to actors_path
+   else
+     render :new
+   end
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  private
+
+  def actor_params
+    params.require(:actor).permit(:name,:birth_date, :birth_place, :bio)
+  end
+
 end
