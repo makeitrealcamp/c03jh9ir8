97a7cfcb0cb8ce42fd0ef1da3487eaded2d1f936
shmesa22 <shmesa23@gmail.com>
2017-03-24 14:14:20 -0500

    Agrega la ruta 'actors/new'

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,19 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    @actor.save
+    redirect_to actors_path
+  end
+
+  private
+  def actor_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/actors', to: 'actors#index'
+  get '/actors/new', to: 'actors#new'
+  post '/actors/new', to: 'actors#create'
 end
