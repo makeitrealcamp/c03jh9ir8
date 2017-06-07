e2bfaf63aa01a4c02f5cd0df694f119f10439c2c
cristian-castrillon <cristian.castrillon@gmail.com>
2017-06-07 11:04:42 -0500

    [MOD] Se agreg la funcionalidad de 'Crear Actor'

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,22 @@ class ActorsController < ApplicationController
   def show
     @actors = Actor.all
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to(actors_path)
+    else
+      @errors = @actor.errors.full_messages
+      render "actors/new"
+    end
+  end
+
+  def actor_params    
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,5 @@ Rails.application.routes.draw do
   #   end
 
   get "actors", to: "actors#show"
+  resources :actors
 end
