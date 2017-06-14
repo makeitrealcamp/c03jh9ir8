e342ee7a53f54aaaf09529c0dbce8c241447cdda
dguilar1994 <andrea@MacBook-Pro-de-Andrea.local>
2017-06-14 15:54:29 -0500

    Agregar solucion del reto 4 de rails

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,17 @@ class ActorsController < ApplicationController
   def actors
     @actors = Actor.all
   end
+  def new
+    @actor = Actor.new
+  end
+  def crear
+    actor = params.require(:actor)
+    .permit(:name, :bio, :birth_date, :birth_date, :death_date, :death_place, :image_url)
+    @actor = Actor.new actor
+    if @actor.save
+      redirect_to actors_path
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'actors' => 'actors#actors'
+  get 'new' => 'actors#new'
+  post 'actors' => 'actors#crear'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
