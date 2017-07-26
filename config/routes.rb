455ed156b295ee9a7112c7b7915d07e15ae42227
Mauricio Lancheros <lucalan66@gmail.com>
2017-07-26 18:13:30 -0500

    commit2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   get '/actors', to: 'actors#index' 
-  get 'actors/new', to: 'actors#new'
-  post 'actors', to: 'actors#create'
+  get '/actors/new', to: 'actors#new'
+  post '/actors', to: 'actors#create'
   #get 'actors/:id' to: 'actors#show' as: actors_show
 
 
