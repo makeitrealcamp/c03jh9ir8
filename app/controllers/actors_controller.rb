0bb603d4f68cde0aaebaa60800ef5b3c9877eca5
eduak2 <evizcaino295@gmail.com>
2017-07-31 16:22:41 -0500

    Chance vinculo

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,7 +14,7 @@ class ActorsController < ApplicationController
         if @actors.save
             redirect_to actors_path, flash: { success: 'The post has been created' }
         else
-            render :index
+            render :new
         end
     end
 
