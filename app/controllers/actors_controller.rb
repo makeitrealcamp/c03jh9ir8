06ab2a1ea87a87f7027e4304d927370a2e4f53a0
jmirav <jmira420@gmail.com>
2017-11-12 21:38:34 -0500

    module for actors creation

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,18 @@ class ActorsController < ApplicationController
     @actors = Actor.all
     # @actors_num = Actor.count
   end
+
+    def new
+    @actor = Actor.new
+  end
+
+  def create
+    actor = Actor.create(actor_params)
+    redirect_to actors_path
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name, :image_url, :bio, :birth_date, :birth_place, :death_date, :death_place)
+  end
+
 end
