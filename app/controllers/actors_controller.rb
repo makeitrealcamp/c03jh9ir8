49944a980563aad7dff0a6c9ecd492a55ac11276
Fermedina <fermed28@gmai.com>
2017-12-02 16:02:28 -0500

    Create actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,20 @@ class ActorsController < ApplicationController
   def index
   	@actors = Actor.all
   end
+  def new
+  	@actor= Actor.new
+  end
+
+   def create
+  	actor= Actor.new(actor_params)
+  	if actor.save
+  		redirect_to actors_path
+  	else
+  	end
+
+  end
+
+  def actor_params
+  	params.require(:actor).permit(:name,:image_url,:bio,:birth_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get '/actors', to: 'actors#index'
+  get '/actors', to: 'actors#index', as: 'actors'
 
+  get '/actors/new', to: 'actors#new', as: 'actor_new'
+
+  post '/actors', to: 'actors#create'
+  
+  #patch '/actors'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
