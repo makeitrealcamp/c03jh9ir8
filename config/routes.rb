3c883d5e5fe505e3d44b87aa4a1b38295e33b454
leotangram <leotangram@gmail.com>
2017-12-02 15:33:59 -0500

    Crear Actores

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
   get '/actors', to: 'actors#index'
+
+  get '/actors/new'
+
+  post '/actors', to: 'actors#create'
+
+  get '/actors', to: 'actors#index' 
+
 end
+
+	
