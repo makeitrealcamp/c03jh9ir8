778615f2ce841d7bd3f869ba4afad878baa0075a
Fermedina <fermed28@gmai.com>
2017-12-02 17:54:39 -0500

    refactor new_author_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/actors', to: 'actors#index', as: 'actors'
 
-  get '/actors/new', to: 'actors#new', as: 'actor_new'
+  get '/actors/new', to: 'actors#new', as: 'new_actor'
 
   post '/actors', to: 'actors#create'
   
