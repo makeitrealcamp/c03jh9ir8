2eeb567160bc83547a74534a71af128026bf5916
jairorjuela <genius85862@gmail.com>
2017-12-02 21:43:08 -0500

    modificacion reto 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :actors
-  #get '/actors', to:"actors#index"
+  get '/actors', to: "actors#index"
+
+  get '/actors/new', to: "actors#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
