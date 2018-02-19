d2c1de7b59c3c8e963d9cd5b936c57823633dff3
GitJay37 <jeisondejesus@hotmail.es>
2018-02-19 17:20:29 -0500

    todo actualizado

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,22 @@ class ActorsController < ApplicationController
 	def index
 		@actors = Actor.all
   	end
+
+  	def new
+  	  @actor = Actor.new
+  	end
+
+  	def create
+  	  @actor = Actor.new(actor_params)
+  	  if @actor.save
+  	    redirect_to actors_path, notice: "El actor fue publicado con éxito"
+  	  else
+  	    render :new
+  	  end
+  	end
+
+  	private
+  	  def actor_params
+  	    params.require(:actor).permit(:name, :image_url, :bio)
+  	  end
 end
