db6c9fde73089c79f7621d14b9d6413179b8a772
william perez <williampbeltranprogramador@gmail.com>
2018-03-21 12:31:12 -0500

    implementa la funcionalidad crear actores con sus rutas respectivas

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,24 @@
 class ActorsController < ApplicationController
 	def index
 		@actors = Actor.all
+	end
+
+	def new
+		@actor = Actor.new 
+	end
+
+	def create
+		@actor = Actor.new(params_application)
+		if @actor.save
+			redirect_to actors_path
+		else
+			render :new
+		end
+		
+	end
+	private
+	def params_application
+		params.require(:actor).permit(:name,:bio,:birth_date,:birth_place,:image_url,:alive,:death_date,:death_place)
 		
 	end
 end
