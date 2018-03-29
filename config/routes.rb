403c3df803d2cd3caed174462518eb8080b25c40
capaisa12 <capaisa12@gmail.com>
2018-03-29 12:46:21 -0500

    Se modifica el link con new_actors_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get '/actors', to: 'actors#index'
-  get '/actors/new', to: 'actors#new'
+  get '/actors/new', to: 'actors#new', as: 'new_actors'
   post '/actors', to: 'actors#create'
 
 
