8edb69d58f9412b941aa315467ee566aafee869b
capaisa12 <capaisa12@gmail.com>
2018-03-29 12:51:56 -0500

    Se corrige la variable por new_actor_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get '/actors', to: 'actors#index'
-  get '/actors/new', to: 'actors#new', as: 'new_actors'
+  get '/actors/new', to: 'actors#new', as: 'new_actor'
   post '/actors', to: 'actors#create'
 
 
