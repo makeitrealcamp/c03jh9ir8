7b4d9effa8a61caed090401705450c17e155b601
Orferus <david.nvalderrama@gmail.com>
2018-04-03 20:29:38 -0500

    Create actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,4 +4,21 @@ class ActorsController < ApplicationController
     @actors = Actor.all
   end
 
+  def create
+    actor_params =  params.require(:actor).permit!
+    if actor_params["death_date"].present? && actor_params["death_place"].present?
+      actor_params.merge!({"alive" => false})
+    else
+      actor_params["alive"] = true
+    end
+    actor = Actor.new(actor_params)
+    if actor.save
+      redirect_to actors_path
+    end
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
 end
