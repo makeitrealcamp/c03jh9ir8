8a599abee20752046977cf0f60528d6b5383ea91
Icorrea7 <isacorrea07@gmail.com>
2018-05-28 14:42:53 -0500

    new/create

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,16 @@ class ActorsController < ApplicationController
   def index
     @actors= Actor.all
   end
+  def new
+    @actor = Actor.new
+  end
+  def create
+    @actor = Actor.create(actor_params)
+    redirect_to actors_path
+  end
+
+  private
+  def actor_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url)
+  end
 end
