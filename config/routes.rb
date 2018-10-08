0097c121264a5850ef71cd5688bb69fa35fae409
garoso <david.fd.251@gmail.com>
2018-10-08 14:20:02 -0500

    Define Create and New route for actors, do index root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'actors/' => 'actors#index'
+  root 'actors#index'
+  
+  resource :actors, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
