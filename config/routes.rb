02fdbe9f663c8fc45e090f440afcdc005c32a2ce
khris <khris56@hotmail.com>
2018-10-11 19:21:57 -0500

    Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get '/actors', to: 'actors#index'
-  get '/actors/new', to: 'actors#new', as: 'new_actor'
-  post '/actors', to: 'actors#create'
+resources :actors
 end
