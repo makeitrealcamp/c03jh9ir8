d4f9e08ddf976a596994a0315bf73a5f36f99d33
Andrés Soto <andres.soto.h@outlook.com>
2018-11-04 15:22:25 -0500

    Initial commit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,30 @@
 class ActorsController < ApplicationController
+
+  def index
+    @actors = Actor.all
+  end
+
+  def create
+    @actors = Actor.create!(actor_params)
+    render json: @actors
+  end
+
+  def show
+    @actor = Actor.find(actor_params[:id])
+  end
+
+  def update
+    @actor.update(actor_params)
+  end
+
+  def destroy
+    @actor.destroy
+  end
+
+  private
+
+  def actor_params
+    params.permit(:id)
+  end
+
 end
