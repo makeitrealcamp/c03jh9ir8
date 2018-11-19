a7627d919abc7eab91a009c1fb8d82f990554378
Alfredo Sumosa <asumosa1993@gmail.com>
2018-11-19 01:43:23 -0500

    Add Create Actor feature

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,18 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(user_params)
+    @actor.save
+    redirect_to '/actors'
+  end
+
+  def user_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+  end
 end
