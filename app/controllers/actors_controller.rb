6278ba2a35b23aec75b792df632ab67f6852a42b
Roberto <robpalacios1@hotmail.com>
2018-11-30 20:04:19 -0500

    Crea la ruta Crea actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,33 @@ class ActorsController < ApplicationController
 	def index
 		@actors = Actor.all
 	end
+
+
+
+	def show
+		@actor = Actor.find(params[:id])
+	end
+
+
+
+	def new
+		@actor = Actor.new
+	end
+
+
+
+	def create
+		@actor =Actor.new(actor_params)
+		if @actor.save
+		   redirect_to actors_path, notice: "El Actor fue publicado con exito"
+		else
+			render :new
+		end
+	end
+
+
+
+	def actor_params
+		params.require(:actor).permit(:name)	
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   get '/actors', to: 'actors#index'
+  get '/actors/new', to: 'actors#new', as: 'new_actor'
+  post '/actors', to: 'actors#create'
+  get '/actors/:id', to: 'actors#show', as: 'actor'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
