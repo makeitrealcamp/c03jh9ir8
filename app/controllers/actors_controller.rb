50e390f37af78cad047a62988924793da0c1bc72
Egojose <josemicrobiologo@gmail.com>
2019-02-21 14:29:21 -0500

    se crea metodo y ruta para crear actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,6 +7,20 @@ class ActorsController < ApplicationController
       @actor = Actor.new
     end
 
+    def create
+      @actor = Actor.new(actor_params)
+      if @actor.save
+        redirect_to actors_path, notice: "El actor se guardó con éxito"
+      else
+        render :new
+      end
+    end
+
+    private
+      def actor_params
+        params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+      end
+
     def edit
       # @actors = Actor.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/actors/new', to: 'actors#new'
 
+  post '/actors', to: 'actors#create'
+
   get '/actors/:id/edit', to: 'actors#edit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
