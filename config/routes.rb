cac66d0c8f6ab04534cbbd5b1085eb7a3c29082b
Egojose <josemicrobiologo@gmail.com>
2019-02-21 18:43:35 -0500

    se crea boton que dirige a crear actor

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/actors', to: 'actors#index'
 
-  get '/actors/new', to: 'actors#new'
+  get '/actors/new', to: 'actors#new', as: 'new_actor'
 
   post '/actors', to: 'actors#create'
 
