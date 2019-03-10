dda16ce7f410fc1bcc2bd566d89e015cea511b5a
Eric Suescun <edsuescun@gmail.com>
2019-03-09 22:48:39 -0500

    Implementada la ruta /actors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
 
-
+  root 'actors#index'
 
   get '/actors/index'
 
+  get '/actors' => 'actors#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
