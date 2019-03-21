de8300dbbba897d9f86e930ddd2688908f2e7828
andres-martin <andresmartin266@gmail.com>
2019-03-20 19:59:53 -0500

    [feature] new functionality created

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,20 @@ class ActorsController < ApplicationController
     def index
         @actors = Actor.all        
     end
+
+    def new
+        @actor = Actor.new
+    end
+
+    def create
+        @actor = Actor.create(actor_params)
+        redirect_to actors_path
+    end
+
+    def actor_params
+        params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive)
+    end
+    
+       
+    
 end
