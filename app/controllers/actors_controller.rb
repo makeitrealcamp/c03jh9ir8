7a93737d66d603c80de51fd0c533d24ceb075f2d
John <johnbaironlr@gmail.com>
2019-03-29 11:25:38 -0500

    creando actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,5 +3,16 @@ class ActorsController < ApplicationController
     @actors = Actor.all
   end
 
-  
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    actor = User.create(user_params)
+    redirect_to :index
+  end
+
+  def actor_params
+     params.require(:actor).permit(:image_url, :name, :bio, :birth_date, :birth_place)
+  end
 end
