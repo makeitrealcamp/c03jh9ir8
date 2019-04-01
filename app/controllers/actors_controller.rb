4d54ec65ef6cd0e4b84014e42c7e497b42bae5a2
sebastian argaez <sargaezu@gmail.com>
2019-03-31 22:21:52 -0500

    Add New

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,6 +2,25 @@ class ActorsController < ApplicationController
 
 def index
 	@actors = Actor.all
+	
+end
+
+def new
+	@actor = Actor.new
+end
+
+def create
+ @actor = Actor.new(actor_params)
+  if @actor.save
+    redirect_to actors_path
+  else
+    render :new
+  end
+end
+
+private
+  def actor_params
+    params.require(:actor).permit(:image_url, :name, :bio, :birth_date, :birth_place, :death_date, :death_place)
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
 get '/actors', to: 'actors#index'
+get '/new', to: 'actors#new'
+post '/actors', to: 'actors#create'
 
 end
