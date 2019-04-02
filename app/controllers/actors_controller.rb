512ab76a0490faaf4464ffc4d8dbb1afde51b873
Julio Bertty <julio.bertty@gmail.com>
2019-04-01 23:06:01 -0500

    Añadiendo la funcion crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,4 +4,23 @@ class ActorsController < ApplicationController
         @actors = Actor.all
     end
 
+    def new
+        @actor = Actor.new
+        
+    end
+
+    def create
+        @actor = Actor.new(actor_params)
+        redirect_to actors_path
+    end
+
+
+
+
+
+    private
+    def actor_params
+      params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :death_date, :death_place)
+    end
+
 end
