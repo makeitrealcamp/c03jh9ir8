ce757c5aece3ec269e966b847cde15e624a2a137
Samuel Rendon G <s.redon.g@gmail.com>
2019-04-02 17:42:05 -0500

    actores 4

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,18 +1,28 @@
 class ActorsController < ApplicationController
-  def index
-  @actors = Actor.all
-  @a= @actors.each do |e|
-      e
-  end
-  end
+      def index
+      @actors = Actor.all
+      @a= @actors.each do |e|
+          e
+      end
+      end
 
-  def new
-  @actor = Actor.new
-  end
+      def new
+      @actor = Actor.new
+      end
 
-  def create
-  actor = Actor.create
-  redirect_to actors_path
-  end
+      def create
+           @actor = Actor.new(actor_params)
+           if @actor.save
+             redirect_to actors_path
+           else
+             render new
+           end
+      end
+
+  private
+
+    def actor_params
+      params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+    end
 
 end
