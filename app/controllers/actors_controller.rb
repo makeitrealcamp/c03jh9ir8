1bc2ed09e0fac4cf20c22d17302fb0e9c4c805be
karenahv <karenahv@gmail.com>
2019-04-12 17:26:23 -0500

    crear actores2

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,7 +4,20 @@ class ActorsController < ApplicationController
     @Actors=Actor.all
   end
 
+  def show
+    @Actor=Actor.find(params[:id])
+  end
+
    def new
      @Actor = Actor.new
    end
+
+   def create
+     actor = Actor.create(actor_params)
+     redirect_to actor_path(actor)
+   end
+
+   def actor_params
+     params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+  end
 end
