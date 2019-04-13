7b38ccc12906617846443a3ffeba500f058554d2
Camilo <munozmarin.camilo@gmail.com>
2019-04-13 13:04:58 -0500

    branch crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,4 +4,21 @@ class ActorsController < ApplicationController
   @actors = Actor.all
   end
 
+  def new
+    @actor = Actor.new
+    ##tittle y content se renderizan vacios
+  end
+
+  def create
+    @actor = Actor.new
+
+    if @actor.save
+      redirect_to actors_path
+    else
+      render :new
+    end
+  end
+
 end
+
+
