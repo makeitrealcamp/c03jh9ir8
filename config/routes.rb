8b6af2a6912e3f725625834e4dcc582c05b3bd40
Jorge Betancur <jabetancur12@gmail.com>
2019-04-16 16:35:16 -0500

    se cran rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # get "/actors", to: "actors#index"
-  # get "/actors/new", to: "actors#new"
-  #  post "/actors", to: "actors#create"
+  get "/actors", to: "actors#index"
+  get "/actors/new", to: "actors#new"
+   post "/actors", to: "actors#create"
 
   root "actors#index"
-  resources :actors
+  #resources :actors
 
 end
