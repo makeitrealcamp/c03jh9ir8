9030ddde0cbcc733d12eb08a3a6437e8531d0be2
Liliana_Rache <lilianarache@gmail.com>
2019-04-16 21:56:48 -0500

    Crea funcionalidad para crear actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,18 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+  def new
+
+  end
+  def create
+    actornew = Actor.new(actor_params)
+    if actornew.save
+      redirect_to actors_path
+    end
+  end
+    private
+  def actor_params
+    params.require(:actor).permit(:image_url,:name,:bio,:birth_date,:birth_place,:death_date,:death_place)
+  end
+
 end
