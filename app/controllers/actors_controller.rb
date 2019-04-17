cd5d9e30fa6bb7f9f9f305741b78ff8386c47fdc
Ozkar09 <oscarbeltran99@hotmail.com>
2019-04-16 21:56:45 -0500

    Se crea funcionalidad para añadir actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,20 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+
+  def new
+  end
+
+  def create
+    actor = Actor.new(actor_params)
+    if actor.save
+      redirect_to actors_path
+    end
+  end
+
+  private
+  def actor_params
+    params.require(:actor).permit(:image_url, :name, :bio, :birth_date, :birth_place, :birth_date, :birth_place)
+  end
+
 end
