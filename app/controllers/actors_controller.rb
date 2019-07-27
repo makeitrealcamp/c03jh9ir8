df33d55ced012f6ac1c344fc5e9f385ea17e0fab
santiagocardo80 <santiagocardo80@gmail.com>
2019-07-27 11:37:18 -0500

    creando actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,23 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.create(actor_params)
+
+    if @actor.save
+      redirect_to actors_path
+    else
+      render :new 
+    end
+  end
+
+  private
+    def actor_params
+      params.require(:actor).permit(:image_url, :name, :bio, :birth_date, :death_date, :death_place)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/actors', to: 'actors#index'
+  get 'actors', to: 'actors#index'
+  resources :actors
 end
