a012f37feb02816f234595f2c9bf6a2a97f3dc27
Dianadhu <dimidusa@gmail.com>
2019-11-12 10:21:19 -0500

    Sol

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -13,5 +12,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
