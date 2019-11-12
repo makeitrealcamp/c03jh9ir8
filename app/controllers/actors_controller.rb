a4363eb3d547ef12889b282f09e6b58f4bc59134
Dianadhu <dimidusa@gmail.com>
2019-11-11 21:40:42 -0500

    solution

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,26 @@
 class ActorsController < ApplicationController
+
+
+    def index
+        @actors = Actor.all
+        render :actors
+    end
+
+
+
+    def new
+        @actor = Actor.new
+    end
+
+
+    def create
+        @actor = Actor.create(actor_params)
+        redirect_to actors_path
+    end
+
+
+     private
+     def actor_params
+        params.require(:actor).permit(:actor, :name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+     end 
 end
